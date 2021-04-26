34192baafc223e2005f6d98935ed6739115d3fe7
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-04-26 07:53:41 +0100

    Update UserService.ts

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,13 @@
+import { getCustomRepository } from "typeorm"
 import { UsersRepository } from "../repository/UsersRepository"
 
 
 class UserService{
 
 async create(email:string){
-  const usersRepository = new UsersRepository()
+  const usersRepository =  getCustomRepository(UsersRepository)
 
-  const userExists = await usersRepository.findOne(email)
+  const userExists = await usersRepository.findOne({email})
 }
 }
 
